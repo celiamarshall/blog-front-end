cb19e13bcb5a3c7e629b1fb33d6b46650ef4a28f
Celia Marshall <celia.m@gmail.com>
2018-11-11 14:13:26 -0800

    fixed bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,42 @@
 const { getPosts, deletePost, addPost, editPost, getPost } = require('./posts')
 const { optionsTemplate, blogPostTemplate } = require('./templates')
 
-//determine what user sees
+const dropdown = document.querySelector('.custom-select')
 const blogPost = document.querySelector('.post')
 const form = document.querySelector('.form-new')
 const editForm = document.querySelector('.form-edit')
-
 const postButton = document.querySelector('.new-post')
 
+//initial render of data
+getPosts()
+  .then((response) => {
+    renderOptions(response.data)
+    addButtonEvents()
+  })
+
+//show form
 postButton.addEventListener('click', (event) => {
-  form.classList.remove('hidden')
   editForm.classList.add('hidden')
   blogPost.classList.add('hidden')
+  form.classList.remove('hidden')
 })
-
-const dropdown = document.querySelector('.custom-select')
-
-dropdown.addEventListener('change', (event) => {
-  form.classList.add('hidden')
-  editForm.classList.add('hidden')
-  blogPost.classList.remove('hidden')
-})
-
 
 //add
 form.addEventListener('submit', (event) => {
   event.preventDefault()
+  if (!event.target.title.value || !event.target.date.value || !event.target.content.value) {
+    document.querySelector('.alert').classList.remove('hidden')
+    return
+  }
+
+  let errorMessage = null
+
   const newPost = {
     title: event.target.title.value,
     date: event.target.date.value,
     content: event.target.content.value
   }
+
   addPost(newPost)
     .then((response) => {
       return getPosts()
@@ -38,16 +44,66 @@ form.addEventListener('submit', (event) => {
     .then((response) => {
       renderOptions(response.data)
     })
+    .catch((error) => {
+      form.classList.add('hidden')
+      blogPost.classList.remove('hidden')
+      errorMessage = error.response.data.error.message[0]
+      blogPost.innerHTML = errorMessage
+    })
 
-  event.target.title.value = ''
-  event.target.date.value = ''
-  event.target.content.value = ''
+  if (!errorMessage) {
+    event.target.title.value = ''
+    event.target.date.value = ''
+    event.target.content.value = ''
 
-  form.classList.add('hidden')
-  blogPost.classList.remove('hidden')
-
-  blogPost.innerHTML = '<p>Post added!</p>'
+    form.classList.add('hidden')
+    blogPost.classList.remove('hidden')
+    blogPost.innerHTML = '<p>Post added!</p>'
+    document.querySelector('.alert').classList.add('hidden')
+  }
 })
+
+
+
+
+//edit and delete button events
+function addButtonEvents() {
+  const editPostButton = document.querySelector('.edit-post')
+  
+  editPostButton.addEventListener('click', (event) => {
+    form.classList.add('hidden')
+    blogPost.classList.add('hidden')
+    editForm.classList.remove('hidden')
+    
+    getPost(dropdown.value)
+    .then((response) => {
+      editFormTitle.value = response.data.data.title
+      editFormDate.value = response.data.data.date
+      editFormContent.value = response.data.data.content
+    })
+  })
+  
+  //delete
+  const remove = document.querySelector('.delete-post')
+  
+  remove.addEventListener('click', (event) => {
+    let errorMessage = null
+    deletePost(dropdown.value)
+    .then((response) => {
+      return getPosts()
+    })
+    .then((response) => {
+      renderOptions(response.data)
+    })
+    .catch((error) => {
+      errorMessage = error.response.data.error.message[0]
+      blogPost.innerHTML = errorMessage
+    })
+    if (!errorMessage) {
+      blogPost.innerHTML = '<p>Post deleted!</p>'
+    }
+  })
+}
 
 
 //edit
@@ -56,6 +112,7 @@ const editFormDate = document.querySelector('#dateEdit')
 const editFormContent = document.querySelector('#contentEdit')
 
 editForm.addEventListener('submit', (event) => {
+  let errorMessage = null
   event.preventDefault()
   const updatedPost = {
     title: event.target.titleEdit.value,
@@ -70,58 +127,33 @@ editForm.addEventListener('submit', (event) => {
     .then((response) => {
       renderOptions(response.data)
     })
+    .catch((error) => {
+      editForm.classList.add('hidden')
+      blogPost.classList.remove('hidden')
+      errorMessage = error.response.data.error.message[0]
+      blogPost.innerHTML = errorMessage
+    })
 
-  blogPost.classList.remove('hidden')
-  editForm.classList.add('hidden')
-
-  blogPost.innerHTML = '<p>Post updated!</p>'
+  if (!errorMessage) {
+    blogPost.classList.remove('hidden')
+    editForm.classList.add('hidden')
+    blogPost.innerHTML = '<p>Post updated!</p>'
+  }
 })
 
 
-function addButtonEvents() {
-  const editPostButton = document.querySelector('.edit-post')
-
-  editPostButton.addEventListener('click', (event) => {
-    form.classList.add('hidden')
-    blogPost.classList.add('hidden')
-    editForm.classList.remove('hidden')
-
-    getPost(dropdown.value)
-      .then((response) => {
-        editFormTitle.value = response.data.data.title
-        editFormDate.value = response.data.data.date
-        editFormContent.value = response.data.data.content
-      })
-  })
-
-  //delete
-  const remove = document.querySelector('.delete-post')
-
-  remove.addEventListener('click', (event) => {
-    deletePost(dropdown.value)
-      .then((response) => {
-        return getPosts()
-      })
-      .then((response) => {
-        renderOptions(response.data)
-      })
-
-    blogPost.innerHTML = '<p>Post deleted!</p>'
-  })
-}
-
 function renderOptions(posts) {
-  const dropdown = document.querySelector('.custom-select')
-  const blogPost = document.querySelector('.post')
-
   const optionsHTMLArray = posts.map(post => optionsTemplate(post))
 
-  dropdown.innerHTML = ''
+  //dropdown.innerHTML = ''
 
-  dropdown.innerHTML = '<option selected>Posts</option>' + optionsHTMLArray.join('')
+  dropdown.innerHTML = optionsHTMLArray.join('')
 
   dropdown.addEventListener('change', (event) => {
-    blogPost.innerHTML = ''
+    form.classList.add('hidden')
+    editForm.classList.add('hidden')
+    blogPost.classList.remove('hidden')
+    //blogPost.innerHTML = ''
 
     getPost(dropdown.value)
       .then((response) => {
@@ -129,15 +161,9 @@ function renderOptions(posts) {
         addButtonEvents()
       })
   })
-
 }
 
-//initial render of data
-getPosts()
-  .then((response) => {
-    renderOptions(response.data)
-    addButtonEvents()
-  })
+
 
 
 
